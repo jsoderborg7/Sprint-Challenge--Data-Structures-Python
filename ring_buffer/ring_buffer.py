330853ef81774b06c440d20df9c80a3312fb587f
Jenn Soderborg <jsoderborg7@gmail.com>
2020-01-31 09:49:47 -0700

    ring buffer mvp complete

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,14 +8,31 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+    # Ok, so since ring buffers have a specific capacity, the size of the DLL has to be at capacity
+    # We'll start with the buffer when it hasn't reached the limit yet
+        if self.storage.length != self.capacity:
+            self.current = self.storage.head
+            self.storage.add_to_tail(item)
+        else:
+    # If we are at capacity:
+    # start by overwriting the current node
+            self.current.value = item
+    # and if we're at the tail, we need to come back around to the head
+            if self.current is self.storage.tail:
+                self.current = self.storage.head
+            else:
+    # if we weren't at the tail, update the position to the next node
+                self.current = self.current.next
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
         # TODO: Your code here
-
+        current = self.storage.head
+        while current:
+            list_buffer_contents.append(current.value)
+            current = current.next
         return list_buffer_contents
 
 # ----------------Stretch Goal-------------------
